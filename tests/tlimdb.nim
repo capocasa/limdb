f8ff0e6268327afb3849dbbd78f3c4053412a022
Carlo Capocasa <carlo@capocasa.net>
2023-03-30 23:13:29 +0200

    use mutable/unmutable variables like table and add more procs like in std/tables

--- tests/tlimdb.nim
+++ tests/tlimdb.nim
@@ -8,9 +8,9 @@
 import os
 import limdb
 
-let testLocation = getTempDir() / "tlimdb"
+var testLocation = getTempDir() / "tlimdb"
 removeDir(testLocation)
-let db = initDatabase(testLocation)
+var db = initDatabase(testLocation)
 
 db["foo"] = "bar"
 assert db["foo"] == "bar", "write and read back"
@@ -25,13 +25,13 @@ assert not ("foo" in db), "not in syntax"
 assert len(db) == 0, "length"
 
 block:
-  let t = db.initTransaction()
+  var t = db.initTransaction()
   t["fuz"] = "buz"
   assert len(t) == 1, "length with transaction"
   t.commit()
 
 block:
-  let t = db.initTransaction()
+  var t = db.initTransaction()
   assert t["fuz"] == "buz", "write and read back with transaction"
   assert t.hasKey("fuz"), "key exists op with ransaction"
   assert "fuz" in t, "in syntax"
@@ -39,19 +39,19 @@ block:
   t.reset()
 
 block:
-  let t = db.initTransaction()
+  var t = db.initTransaction()
   t.del("fuz")
   t.commit()
 
 block:
-  let t = db.initTransaction()
+  var t = db.initTransaction()
   doAssertRaises(Exception): discard t["fuz"]
   assert not t.hasKey("fuz"), "key does not exist op with transaction"
   assert not ("fuz" in t), "not in syntax with transaction"
   assert len(t) == 0, "length with transaction"
   t.reset()
 
-let db2 = db.initDatabase("db2")
+var db2 = db.initDatabase("db2")
 doAssertRaises(Exception): discard db2["foo"]
 
 db2["foo"] = "bar"
@@ -60,27 +60,27 @@ db2.del("foo")
 doAssertRaises(Exception): discard db2["foo"]
 
 block:
-  let t = db2.initTransaction()
+  var t = db2.initTransaction()
   t["fuz"] = "buz"
   t.commit()
 
 block:
-  let t = db2.initTransaction()
+  var t = db2.initTransaction()
   assert t["fuz"] == "buz", "write and read back with transaction on named database"
   t.reset()
 
 block:
-  let t = db2.initTransaction()
+  var t = db2.initTransaction()
   t.del("fuz")
   t.commit()
 
 block:
-  let t = db2.initTransaction()
+  var t = db2.initTransaction()
   doAssertRaises(Exception): discard t["fuz"]
   t.reset()
 
 block:
-  let t = db2.initTransaction()
+  var t = db2.initTransaction()
   t["foo"] = "bar"
   t["fuz"] = "buz"
   t["fooo"] = "baar"
@@ -94,7 +94,7 @@ block:
   assert s == @["foo", "fooo", "fuuz", "fuz"], "iterate over keys in order"
 
 block:
-  let t = db2.initTransaction()
+  var t = db2.initTransaction()
   var s:seq[string]
   for key in t.keys:
     s.add(key)
@@ -108,7 +108,7 @@ block:
   assert s == @["bar", "baar", "buuz", "buz"], "iterate over values in order"
 
 block:
-  let t = db2.initTransaction()
+  var t = db2.initTransaction()
   var s:seq[string]
   for value in t.values:
     s.add(value)
@@ -141,15 +141,46 @@ block:
   assert len(db2) == 4, "count length"
 
 block:
-  let db3 = db.initDatabase("db3")
+  var db3 = db.initDatabase("db3")
   assert db3.getOrDefault("foo") == "", "key does not exist, use default"
   db3["foo"] = "bar"
   assert db3.getOrDefault("foo") == "bar", "key there, use value"
   db3.del("foo")
 
-  let t = db3.initTransaction()
+  var t = db3.initTransaction()
   assert t.getOrDefault("foo") == "", "key does not exist, use default, in transaction"
   t["foo"] = "bar"
   assert t.getOrDefault("foo") == "bar", "key there, use value, in transaction"
   t.reset()
+
+block:
+  var db4 = db.initDatabase("db4")
+  assert db4.hasKeyOrPut("foo", "bar") == false, "returns false if key not in database"
+  assert db4["foo"] == "bar", "value was set"
+  assert db4.hasKeyOrPut("foo", "fuz"), "returns true if key in database"
+  assert db4["foo"] == "bar", "value was not set, still previous value"
+  db4.del("foo")
+
+  var t = db4.initTransaction
+  assert t.hasKeyOrPut("foo", "bar") == false, "returns false if key does not exist in transaction"
+  assert t["foo"] == "bar", "value was set"
+  assert t.hasKeyOrPut("foo", "fuz"), "returns true if key in database"
+  assert t["foo"] == "bar", "value was not set, still previous value"
+  t.reset()
+
+#[
+block:
+  var db5 = db.initDatabase("db5")
+  var s = db5.mgetOrPut("foo", "bar")
+  assert s == "bar", "value was returned"
+  assert s["foo"] == "bar", "value was put"
+  s.add("fuz")
+  assert s == "barfuz", "return value is mutable"
+  assert db5["foo"] == "bar", "value was not changed in database by changing returned value"
+  var s2 = db5.mgetOrPut("foo", "fuz")
+  assert s2 == "bar", "value in database is returned"
+  s2.add("fuz")
+  assert s2 == "barfuz", "returned value is mutable"
+  assert db5["foo"] == "bar", "value was not changed in database by changing returned value"
+]#
 
